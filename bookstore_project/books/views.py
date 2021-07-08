ff856bf9c246856d4ed051e13b7594b2445c7f12
Joseph <chaofengjojo@hotmail.com>
2021-07-08 20:10:25 +1200

    jason file added

--- bookstore_project/books/views.py
+++ bookstore_project/books/views.py
@@ -1,5 +1,12 @@
-from django.http import HttpResponse
+from typing import ContextManager
+from django.shortcuts import render
+import json
+
+# booksData = open("/Users/chaof/Desktop/Django_Project_Demo/bookstore_project/books.json").read()
+booksData = open('C:/Users/chaof/Desktop/Django_Project_Demo/bookstore_project/books.json').read()
+data = json.loads(booksData)
 
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+    context = {'books': data}
+    return render(request, 'books/index.html', context)
