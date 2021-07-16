b4b88121f28f28d820ba1077778ff2620145c61b
Joseph <chaofengjojo@hotmail.com>
2021-07-16 15:15:06 +1200

    template

--- bookstore_project/books/models.py
+++ bookstore_project/books/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class Book (models.Model):
+    title = models.CharField(max_length=256)
+    pageCount = models.IntegerField(default=0)
+    thumbnailUrl = models.CharField(max_length=256, null=True)
+    shortDescription = models.CharField(max_length=256, null=True)
+    longDescription = models.TextField(null=True)
+ 
--- bookstore_project/books/urls.py
+++ bookstore_project/books/urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index'),
+    path('', views.index, name='book.all'),
+    path('<int:id>', views.show, name ='book.show')
 ]
